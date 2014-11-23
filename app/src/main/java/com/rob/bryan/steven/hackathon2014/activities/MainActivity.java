136d6b31c4a2ab50751e19e34a20eee3ec6de52b
Steven <sluijts@gmail.com>
2014-11-23 12:16:43 +0100

    Changed loginflow

--- app/src/main/java/com/rob/bryan/steven/hackathon2014/activities/MainActivity.java
+++ app/src/main/java/com/rob/bryan/steven/hackathon2014/activities/MainActivity.java
@@ -1,18 +1,20 @@
 package com.rob.bryan.steven.hackathon2014.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.rob.bryan.steven.hackathon2014.R;
-import com.rob.bryan.steven.hackathon2014.activities.BaseActivity;
 
 import io.relayr.RelayrSdk;
 
 
 public class MainActivity extends BaseActivity {
+
+    private MenuItem mLogIn;
+    private MenuItem mLogOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +36,19 @@ public class MainActivity extends BaseActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        mLogIn = menu.findItem(R.id.action_log_in);
+        mLogOut = menu.findItem(R.id.action_log_out);
+
+        if (RelayrSdk.isUserLoggedIn()) {
+            mLogOut.setVisible(true);
+            mLogIn.setVisible(false);
+        } else {
+            mLogOut.setVisible(false);
+            mLogIn.setVisible(true);
+        }
+
+
         return true;
     }
 
@@ -44,11 +59,29 @@ public class MainActivity extends BaseActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+            case R.id.action_log_in:
+                startActivity(new Intent(this, LoginActivity.class));
+                return true;
+            case R.id.action_log_out:
+                logOut();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void logOut() {
+
+        //call the logOut method on the reayr SDK
+        RelayrSdk.logOut();
+
+        mLogOut.setVisible(false);
+        mLogIn.setVisible(true);
+
+        //use the Toast library to display a message to the user
+        Toast.makeText(this, R.string.successfully_logged_out, Toast.LENGTH_SHORT).show();
     }
 }
