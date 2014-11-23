94096f68bc835ed75281e2086bfc9d41f29476f1
Steven <sluijts@gmail.com>
2014-11-23 17:43:46 +0100

    Removed stuff from temperature check

--- app/src/main/java/com/rob/bryan/steven/hackathon2014/services/SensorDataService.java
+++ app/src/main/java/com/rob/bryan/steven/hackathon2014/services/SensorDataService.java
@@ -328,10 +328,7 @@ public class SensorDataService extends IntentService {
                     public void onNext(Object o) {
                         Reading reading = new Gson().fromJson(o.toString(), Reading.class);
                         //Log.d("SensorDataService", reading.temp + "˚C");
-                        if (AlarmManager.checkFridgeTemperature(reading.temp, SensorDataService.this)
-                                || AlarmManager.checkNoiseLevel(reading.snd_level, SensorDataService.this)
-                                || AlarmManager.checkLight(reading.light, SensorDataService.this)
-                                || AlarmManager.checkWindowOpen(reading.prox, SensorDataService.this)) {
+                        if (AlarmManager.checkFridgeTemperature(reading.temp, SensorDataService.this)) {
                             showNotification();
                         }
                     }
